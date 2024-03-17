0abde2ba9c953186a67099589ca047aa6b3e1789
João Victor <joao-victor-o@hotmail.com>
2024-03-17 12:13:18 -0300

    mudando a tabela usada no back

--- src/main/java/com/projetoBD/demo/consultas/ConsultasRepository.java
+++ src/main/java/com/projetoBD/demo/consultas/ConsultasRepository.java
@@ -5,25 +5,28 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
 @Repository //o spring entende que essa classe faz acesso ao BD e traduz as excecoes do BD
 public class ConsultasRepository {
 
-    private static String NOVACONSULTA = "INSERT INTO consultas (nome_paciente, nome_medico, data_consulta, motivo_consulta) VALUES (?, ?, ?, ?)";
-    private static String BUSCARCONSULTA = "SELECT * FROM consultas WHERE id_consulta = ?";
-    private static String DELETARCONSULTA = "DELETE FROM consultas WHERE id_consulta = ?";
-    private static String ATUALIZARCONSULTA = "UPDATE consultas SET nome_paciente = ?, nome_medico = ?, data_consulta = ?, motivo_consulta = ? WHERE id_consulta = ?";
-    private static String LISTARCONSULTAS = "SELECT * FROM consultas";
+    private static String NOVACONSULTA = "INSERT INTO consultasBD (nome_paciente, nome_medico, data_consulta, motivo_consulta) VALUES (?, ?, ?, ?)";
+    private static String BUSCARCONSULTA = "SELECT * FROM consultasBD WHERE id_consulta = ?";
+    private static String DELETARCONSULTA = "DELETE FROM consultasBD WHERE id_consulta = ?";
+    private static String ATUALIZARCONSULTA = "UPDATE consultasBD SET nome_paciente = ?, nome_medico = ?, data_consulta = ?, motivo_consulta = ? WHERE id_consulta = ?";
+    private static String LISTARCONSULTAS = "SELECT * FROM consultasBD";
 
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public void marcarConsulta (ConsultasEntity consulta){
+    public void marcarConsulta(ConsultasEntity consulta) {
         //o metodo update do JDBC serve para scripts SQL de atuailizacao, insercao e delecao
-        jdbcTemplate.update(NOVACONSULTA, consulta.getNomePaciente(), consulta.getNomeMedico(), consulta.getDataConsulta(), consulta.getMotivoConsulta());
+
+        Timestamp horarioConsulta = Timestamp.valueOf(consulta.getDataConsulta());
+        jdbcTemplate.update(NOVACONSULTA, consulta.getNomePaciente(), consulta.getNomeMedico(), horarioConsulta, consulta.getMotivoConsulta());
     }
 
     public Optional<ConsultasEntity> buscarConsultaPorId(Integer id) {
